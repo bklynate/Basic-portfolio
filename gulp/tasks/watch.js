0c875628337d693fbcd7ad275013fb67abe6152b
bklynate <nathanielcfa@gmail.com>
2016-11-03 17:36:32 -0400

    Add Gulp and finished styling header

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  watch('./index.html', function(){
+    browserSync.reload();
+  });
+
+  watch('./content.html', function(){
+    browserSync.reload();
+  });
+
+  watch('./assets/css/**/*.css', function(){
+    gulp.start('cssInject');
+    browserSync.reload();
+  });
+
+});
+
+gulp.task('cssInject',['styles'], function(){
+  return gulp.src("./assets/temp/css/styles.css")
+    .pipe(browserSync.stream());
+});
+
